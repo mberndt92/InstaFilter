b19613ff94dd0d67c9545fe13e3e83b570601cfa
mberndt92 <maximilian_berndt@icloud.com>
2023-04-08 14:41:07 +0900

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Maximilian Berndt on 2023/04/08.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
